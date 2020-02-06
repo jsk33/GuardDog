bd6fe74f7d71da097efbd113c5635dbe50523c4a
jsk33 <jason33kim@gmail.com>
2020-02-06 15:19:44 -0500

    namespaced guardButton.js

--- scripts/guardButton.js
+++ scripts/guardButton.js
@@ -1,25 +1,25 @@
-const input = document.querySelector('input');
+const dogIcon = document.querySelector('.dogIcon');
 
-function setCount(response) {
-    const div = document.createElement('div');
-    div.textContent = `${response.count} images doggofied :)`;
-    document.body.appendChild(div);
+var guardButton = {
+    init: function init() {
+        document.addEventListener('DOMContentLoaded', guardButton.dogIconFunction);
+    },
+    dogIconFunction: function dogIconFunction() {
+        dogIcon.addEventListener("click", guardButton.handleClick, false);
+    },
+    handleClick: function handleClick() {
+        chrome.tabs.query({currentWindow: true, active: true},
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, 'doggofy this page', guardButton.setCount);
+            }
+        )
+    },
+    setCount: function setCount(response) {
+        const div = document.createElement('div');
+        div.textContent = `${response.count} images doggofied :)`;
+        document.body.appendChild(div);
+    }
+
 }
 
-function handleClick() {
-    chrome.tabs.query({currentWindow: true, active: true},
-        function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, 'doggofy this page', setCount);
-        }
-    )
-}
-
-function inputFunction() {
-    input.addEventListener('click', handleClick, false);
-}
-
-function init() {
-    document.addEventListener('DOMContentLoaded', inputFunction);
-}
-
-init();
+guardButton.init();
