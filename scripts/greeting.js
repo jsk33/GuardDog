c22f30cfd27d38442d5b8a0dae927a0e94680122
jsk33 <jason33kim@gmail.com>
2020-02-07 10:03:40 -0500

    namespaced greeting.js and guardList.js

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -5,56 +5,58 @@ const greetingChangeUserBtn = document.querySelector(".greetingChangeUserBtn-js"
 
 const SHOWING_ON = "showing";
 
-function paintGreeting(text) {
-    console.log("painting greeting");
-    greetingForm.classList.remove(SHOWING_ON);
-    greetingMessage.classList.add(SHOWING_ON);
-    greetingMessage.innerText = `Hey ${text}, I'm GuardDog`;
-}
+var greeting = {
+    paintGreeting: function paintGreeting(text) {
+        console.log("painting greeting");
+        greetingForm.classList.remove(SHOWING_ON);
+        greetingMessage.classList.add(SHOWING_ON);
+        greetingMessage.innerText = `Hey ${text}, I'm GuardDog`;
+    },
 
-function handleSubmit(event) {
-    event.preventDefault();
+    handleSubmit: function handleSubmit(event) {
+        event.preventDefault();
+    
+        const newName = greetingInput.value;
+        chrome.storage.local.set({currentUser: newName}, function() {
+            console.log("current user is set to: " + newName);
+        });
+        greeting.paintGreeting(newName);
+    },
 
-    const newName = greetingInput.value;
-    chrome.storage.local.set({currentUser: newName}, function() {
-        console.log("current user is set to: " + newName);
-    });
-    paintGreeting(newName);
-}
+    askForName: function askForName() {
+        console.log("asking for name");
+        greetingForm.classList.add(SHOWING_ON);
+        greetingForm.addEventListener("submit", handleSubmit);
+    },
 
-function askForName() {
-    console.log("asking for name");
-    greetingForm.classList.add(SHOWING_ON);
-    greetingForm.addEventListener("submit", handleSubmit);
-}
-
-function loadName() {
-    let currentUser;
-
-    getCurrentUserPromise().then(result => {
-        currentUser = result;
-        currentUser === '' ? askForName() : paintGreeting(currentUser);
-    })
-}
-
-function getCurrentUserPromise() {
-    return new Promise(resolve => {
-        chrome.storage.local.get({currentUser: ''}, function(items) {
-          resolve(items.currentUser);
+    loadName: function loadName() {
+        let currentUser;
+    
+        getCurrentUserPromise().then(result => {
+            currentUser = result;
+            currentUser === '' ? greeting.askForName() : greeting.paintGreeting(currentUser);
         })
-    })
+    },
+    
+    getCurrentUserPromise: function getCurrentUserPromise() {
+        return new Promise(resolve => {
+            chrome.storage.local.get({currentUser: ''}, function(items) {
+              resolve(items.currentUser);
+            })
+        })
+    },
+
+    changeUserBtnOnClick: function changeUserBtnOnClick(event) {
+        chrome.storage.local.clear();
+        greetingMessage.classList.remove(SHOWING_ON);
+        greeting.askForName();
+    },
+
+    init: function init() {
+        console.log("greeting.js initialized");
+        greeting.loadName();
+        greetingChangeUserBtn.addEventListener("click", changeUserBtnOnClick);
+    }
 }
 
-function changeUserBtnOnClick(event) {
-    chrome.storage.local.clear();
-    greetingMessage.classList.remove(SHOWING_ON);
-    askForName();
-}
-
-function init() {
-    console.log("greeting.js initialized");
-    loadName();
-    greetingChangeUserBtn.addEventListener("click", changeUserBtnOnClick);
-}
-
-init();
+greeeting.init();
--- scripts/guardList.js
+++ scripts/guardList.js
@@ -3,52 +3,53 @@ const guardListInput = guardListForm.querySelector("input");
 const guardListItems = document.querySelector(".guardListItems-js");
 
 
+var guardList = {
+    paintGuardList: function paintGuardList(textArray) {
+        // for each item in the guard list, append a li child to guardListItems
+        
+    },
 
-function paintGuardList(textArray) {
-    // for each item in the guard list, append a li child to guardListItems
+    saveGuardList: function saveGuardList(textArray) {
+        chrome.storage.local.set({"guardList": textArray}, function() {
+            console.log("guardList is set to: " + textArray);
+        })
+    },
+
+    handleSubmit: function handleSubmit(event) {
+        event.preventDefault();
     
-}
+        const newGuardItem = guardListInput.value;
+        guardList.addToGuardList(newGuardItem);
+    },
 
-function saveGuardList(textArray) {
-    chrome.storage.local.set({"guardList": textArray}, function() {
-        console.log("guardList is set to: " + textArray);
-    })
-}
-
-function handleSubmitGuardList(event) {
-    event.preventDefault();
-
-    const newGuardItem = guardListInput.value;
-    addToGuardList(newGuardItem);
-}
-
-function addToGuardList(text) {
-    // append the new item to the string array of guard items
-    let guardList;
+    addToGuardList: function addToGuardList(text) {
+        // append the new item to the string array of guard items
+        let guardList;
+        
+        chrome.storage.local.get("guardList", function(result) {
+            guardList = result;
+        });
     
-    chrome.storage.local.get("guardList", function(result) {
-        guardList = result;
-    });
+        guardList.append(text);
+        guardList.saveGuardList(guardList);
+        guardList.paintGuardList(guardList);
+    },
 
-    guardList.append(text);
-    saveGuardList(guardList);
-    paintGuardList(guardList);
-}
-
-function loadGuardListItems() {
-    let guardList;
+    loadGuardListItems: function loadGuardListItems() {
+        let guardList;
+        
+        chrome.storage.local.get("guardList", function(result) {
+            guardList = result;
+        });
     
-    chrome.storage.local.get("guardList", function(result) {
-        guardList = result;
-    });
+        guardList === undefined ? console.log("empty guard list") : guardList.paintGuardList(guardList);
+    },
 
-    guardList === undefined ? console.log("empty guard list") : paintGuardList(guardList);
+    init: function init() {
+        console.log("guardList.js initialized");
+        guardList.loadGuardListItems();
+        guardListForm.addEventListener("submit", handleSubmit);
+    }
 }
 
-function init() {
-    console.log("guardList.js initialized");
-    loadGuardListItems();
-    guardListForm.addEventListener("submit", handleSubmit);
-}
-
-init();
+guardList.init();
