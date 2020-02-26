67637d30c431aa6a31610314e8bd448a0742e32f
jsk33 <jason33kim@gmail.com>
2020-02-26 16:50:25 -0500

    clean up message passing

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,6 @@
-let intervalDuration;
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message == "doggofy this page") {
-        let images = document.querySelectorAll('img');
-        images.forEach(function(image) {
-            image.removeAttribute('srcset');
-            image.src = "https://www.rover.com/blog/wp-content/uploads/2019/11/shiba-dreamstime-960x540.jpg";
-        });
+        content.doggofy();
     } else {
         intervalDuration = request.gracePeriod;
         console.log(intervalDuration);
@@ -21,28 +15,15 @@ chrome.runtime.sendMessage({message: "are you wasting time?"}, function(response
         content.getGracePeriodPromise().then(result => {
             intervalDuration = result;
             if (intervalDuration === '') {
-                
+                // no need to activate doggofyInterval
             } else {
-                window.setTimeout(doggofyInterval, intervalDuration * 60000);
-                console.log(`this page will doggofy in ${intervalDuration} minutes`);
+                // activate doggofyInterval after 'intervalDuration' minutes
+                window.setTimeout(content.doggofyInterval, content.durationInMinutes(intervalDuration));
             }
-        })
-        
+        });
     }
 });
 
-
-function doggofy() {
-    let images = document.querySelectorAll('img');
-    images.forEach(function(image) {
-        image.removeAttribute('srcset');
-        image.src = "https://www.rover.com/blog/wp-content/uploads/2019/11/shiba-dreamstime-960x540.jpg";
-    });
-}
-
-function doggofyInterval() {
-    setInterval(doggofy, 5000);
-}
 
 var content = {
     getGracePeriodPromise: function getGracePeriodPromise() {
@@ -51,5 +32,22 @@ var content = {
               resolve(items.gracePeriod);
             });
         });
+    },
+
+    doggofy: function doggofy() {
+        let images = document.querySelectorAll('img');
+        images.forEach(function(image) {
+            image.removeAttribute('srcset');
+            image.src = "https://www.rover.com/blog/wp-content/uploads/2019/11/shiba-dreamstime-960x540.jpg";
+        });
+    },
+
+    doggofyInterval: function doggofyInterval() {
+        // doggofies every 5 seconds
+        setInterval(content.doggofy, 5000);
+    },
+
+    durationInMinutes: function durationInMinutes(intervalDuration) {
+        return intervalDuration * 60000;
     }
 }
